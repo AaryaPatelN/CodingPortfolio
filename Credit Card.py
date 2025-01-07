a037e6028b3e680eb58bda1dc2035eecf15ab1cf
AaryaPatelN <136502234+AaryaPatelN@users.noreply.github.com>
2025-01-07 14:40:27 -0500

    Update Credit Card.py

--- Credit Card.py
+++ Credit Card.py
@@ -1,17 +1,17 @@
 from datetime import datetime
-
 transactions = [
     {"amount": 100.50, "location": "New York", "time": "2023-06-20T10:15:00"},
-    {"amount": 500.00, "location": "Los Angeles", "time": "2023-06-20T12:30:00"},
+    {"amount": 500.00, "location": "Los Angeles", "time": "2023-06-20T12:30:00"},  
     {"amount": 890.00, "location": "London", "time": "2023-06-20T15:45:00"},
-    {"amount": 378.66, "location": "Vancouver", "time": "2023-07-20T15:32:00"},
-    {"amount": 245.00, "location": "Miami", "time": "2023-08-20T15:23:00"},
-    {"amount": 175.00, "location": "Charlotte", "time": "2023-08-20T15:16:00"},
-    {"amount": 50.00, "location": "San Francisco", "time": "2023-06-21T09:00:00"},
-    {"amount": 1500.00, "location": "London", "time": "2023-07-15T10:00:00"},
+    {"amount": 378.66, "location": "Vancouver", "time": "2023-07-20T15:32:00"}, 
+    {"amount": 245.00, "location": "Miami", "time": "2023-08-20T15:23:00"},  
+    {"amount": 175.00, "location": "Charlotte", "time": "2023-08-20T15:16:00"},  
+    {"amount": 50.00, "location": "San Francisco", "time": "2023-06-21T09:00:00"}, 
+    {"amount": 1500.00, "location": "London", "time": "2023-07-15T10:00:00"},  
     {"amount": 491.00, "location": "Toronto", "time": "2023-07-21T11:00:00"},
-    {"amount": 1200.00, "location": "Sydney", "time": "2023-08-10T14:00:00"},
-   
+    {"amount": 1200.00, "location": "London", "time": "2023-07-16T14:00:00"},  
+    {"amount": 750.00, "location": "Sydney", "time": "2023-08-11T14:00:00"},  
+    {"amount": 1000.00, "location": "London", "time": "2023-07-17T10:00:00"}   
 ]
 
 # Define rules and transactions
@@ -33,7 +33,7 @@ for transaction in transactions:
         if (
             t["location"] == transaction["location"] and 
             abs((datetime.strptime(t["time"], "%Y-%m-%dT%H:%M:%S") - 
-                 datetime.strptime(transaction["time"], "%Y-%m-%dT%H:%M:%S")).total_seconds()) <= 1800
+                 datetime.strptime(transaction["time"], "%Y-%m-%dT%H:%M:%S")).total_seconds()) <= 86400
         )
     ]
     
